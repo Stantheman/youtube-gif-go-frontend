d54b3e98920d131124a9ee05e5350776fe8c6140
Stan Schwertly <Stan@schwertly.com>
2014-10-22 04:44:57 -0400

    add validation, collect coords for rect, allow url to be passed in

--- js/gif.js
+++ js/gif.js
@@ -3,8 +3,6 @@
 */
 var vid_length = 0;
 var ready = false;
-var bs;
-
 function onYouTubePlayerReady(playerId) {
 	console.log(playerId);
 	ytplayer = document.getElementById("myytplayer");
@@ -13,7 +11,6 @@ function onYouTubePlayerReady(playerId) {
 	ytplayer.mute();
 	ready = true;
 }
-
 function ytStateChange(change) {
 	ytplayer = document.getElementById("myytplayer");
 	console.log("change: " +change + ". vid_length: " + vid_length + ". state: " + change)
@@ -27,8 +24,6 @@ function ytStateChange(change) {
 				if (ready && document.id('firstValueH').value != firstValue) {
 					console.log("totally updating: first: " + firstValue + " current " + document.id('firstValueH').value )
 					ytplayer.seekTo(firstValue, true);
-				//	ytplayer.playVideo()
-				//	ytplayer.pauseVideo();
 				}
 				document.id('firstValueH').set('value', firstValue);
 				document.id('secondValueH').set('value', secondValue);
@@ -37,19 +32,23 @@ function ytStateChange(change) {
 			start: [0, vid_length],
 			precision: 0,
 		});
-	//	ytplayer.playVideo()
-	//	ytlpayer.pauseVideo()
 	}
 }
-
 function gifit(start, dur) {
+	var payload = {
+		"url":"https://www.youtube.com/watch?v=2sSRZsgRy8U",
+		"start":start,
+		"dur": dur,
+	}
+	if ($('#x').val() != "") {
+		payload.cx = $('#x').val()
+		payload.cy = $('#y').val()
+		payload.ch = $('#h').val()
+		payload.cw = $('#w').val()
+	}
 	$.post(
 		"http://97.107.141.47:8080/gifs",
-		{
-			"url":"https://www.youtube.com/watch?v=2sSRZsgRy8U",
-			"start":start,
-			"dur": dur,
-		},
+		payload,
 		function(data) {
 			var oldanswer = ""
 			var interval = setInterval(function(){
@@ -75,67 +74,81 @@ function gifit(start, dur) {
 		}
 	)
 }
-
 function cropit() {
 	var _this = this;
-
 	if(!this.jcropEl) {
 		ytplayer = document.getElementById("myytplayer");
-
 		var el = $('#holder');
 		el.width(ytplayer.width);
 		el.height(ytplayer.height);
-
-		el.Jcrop({}, function() {			
+		el.Jcrop({
+			onSelect: updateCoords,
+		}, function() {
 			var jcropEl = _this.jcropEl = el.parent();
 			_this.jcropping = true;
-
 			jcropEl.css({
 				'background-color': 'transparent',
 				position: 'absolute',
 			});
 		});
-
 	} else if(!this.jcropping) {
 		this.jcropEl.css({
 			'z-index': 600,
 		});
 		this.jcropping = true;
-	} else {	
+	} else {
 		this.jcropEl.css({
 			'z-index': -10,
 		});
 		this.jcropping = false;
 	}
 }
-
-function toggleStatus() {
-    if ($('#toggleElement').is(':checked')) {
-        $('#idOfTheDIV :input').attr('disabled', true);
-    } else {
-        $('#idOfTheDIV :input').removeAttr('disabled');
-    }   
+function updateCoords(c) {
+	$('#x').val(c.x);
+	$('#y').val(c.y);
+	$('#w').val(c.w);
+	$('#h').val(c.h);
 }
-
 function init() {
 	var params = { allowScriptAccess: "always", wmode: "transparent" };
 	var atts = { id: "myytplayer" };
 	swfobject.embedSWF(
-		"http://www.youtube.com/v/2sSRZsgRy8U?enablejsapi=1&playerapiid=ytplayer&version=3&fs=0&iv_load_policy=3",
-		"ytapiplayer", 
-		"425", 
-		"300", 
-		"8", 
-		null, 
-		null, 
-		params, 
+		"http://www.youtube.com/v/2sSRZsgRy8U?enablejsapi=1&playerapiid=ytplayer&version=3&fs=0&iv_load_policy=3&showinfo=0",
+		"ytapiplayer",
+		"425",
+		"300",
+		"8",
+		null,
+		null,
+		params,
 		atts
 	);
-
-	/*Set up the bomb/slider */
-	console.log("in bs, vid_length is: " + vid_length)
 	document.getElementById('gifit').onclick = function(){
 		gifit(document.id('firstValueH').value, document.id('secondValueH').value - document.id('firstValueH').value);
 	}
 	document.getElementById('cropit').onclick = function(){cropit()}
+	$('#url').on('input', function(){
+		//https://www.youtube.com/watch?v=dgKGixi8bp8
+		var input = $(this)
+		var re = /^https:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9_-]{11})$/;
+		var is_legit = re.exec(input.val());
+		if (is_legit) {
+			$("span", input.parent()).removeClass("error_show").addClass("error");
+			swfobject.removeSWF("myytplayer");
+			$("#holder").after('<div id="ytapiplayer"></div>');
+			swfobject.embedSWF(
+				"http://www.youtube.com/v/" + is_legit[1] + "?enablejsapi=1&playerapiid=ytplayer&version=3&fs=0&iv_load_policy=3&showinfo=0",
+				"ytapiplayer",
+				"425",
+				"300",
+				"8",
+				null,
+				null,
+				params,
+				atts
+			);
+		} else {
+			$("span", input.parent()).removeClass("error").addClass("error_show");
+		}
+	});
 }
